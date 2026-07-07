807ff686b92ccbc9995d47d466d208dd4f6ec280
Alyson <165923258+Gridzly@users.noreply.github.com>
2026-07-07 13:33:40 +0800

    changes

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-console.log("Script loaded");
-
+console.log("Theme Script Loaded");
 
 const button = document.getElementById("theme-toggle");
+const icon = button.querySelector("i");
 
-
-button.addEventListener("click", function(){
-
-    console.log("Button clicked");
-
-
+button.addEventListener("click", () => {
     document.body.classList.toggle("dark");
+
+
+    if(document.body.classList.contains("dark")){
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+    }
+    else{
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+
+    }
 
 
 });
