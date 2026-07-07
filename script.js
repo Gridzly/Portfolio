ea64d5d1233951192ff788d06278329f413d21a8
Alyson <165923258+Gridzly@users.noreply.github.com>
2026-07-07 18:07:12 +0800

    added project

--- script.js
+++ script.js
@@ -98,3 +98,41 @@ projects.forEach(project => {
     });
 
 });
+
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+let current = 0;
+
+function showSlide(index){
+
+    slides.forEach(slide=>slide.classList.remove("active"));
+
+    slides[index].classList.add("active");
+
+}
+
+next.addEventListener("click",()=>{
+
+    current++;
+
+    if(current>=slides.length){
+        current=0;
+    }
+
+    showSlide(current);
+
+});
+
+prev.addEventListener("click",()=>{
+
+    current--;
+
+    if(current<0){
+        current=slides.length-1;
+    }
+
+    showSlide(current);
+
+});
