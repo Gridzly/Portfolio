6942e788baa7027decaf82eb7efabd05ebc217e1
Alyson <165923258+Gridzly@users.noreply.github.com>
2026-07-07 13:45:32 +0800

    changes

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const button = document.getElementById("theme-toggle");
 const icon = button.querySelector("i");
 
-button.addEventListener("click",()=>{
+button.addEventListener("click", () => {
+
     document.body.classList.toggle("dark");
 
     if(document.body.classList.contains("dark")){
-        // Dark mode = show sun
+
         icon.classList.remove("fa-moon");
         icon.classList.add("fa-sun");
 
     }else{
-        // Light mode = show moon
+
         icon.classList.remove("fa-sun");
         icon.classList.add("fa-moon");
 
     }
-
 
 });
